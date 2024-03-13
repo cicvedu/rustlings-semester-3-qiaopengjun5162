65d082b4359d60caeb12fc8606dcf048c211c5b3
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-03-13 14:32:25 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,42 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
-    pub student_name: String,
-    pub student_age: u8,
+pub enum Grade {
+    Numeric(f32),
+    Alphabetical(String),
 }
 
-impl ReportCard {
+// 注意：这里使用了泛型参数T和PhantomData来使ReportCard结构体成为一个泛型结构体。
+// 这是因为在Rust中，如果一个泛型结构体没有使用泛型参数，那么它就不能被实例化。
+// 通过使用PhantomData，我们可以在不使用泛型参数的情况下，仍然使ReportCard成为一个泛型结构体。
+pub struct ReportCard<T> {
+    pub grade: Grade,
+    pub student_name: String,
+    pub student_age: u8,
+    // 注意：这里使用了PhantomData来占位，因为T是一个泛型参数，但是我们不需要使用它。
+    // 这样做的目的是保持ReportCard结构体的泛型性，即使它没有使用T作为参数。
+    // 这样可以避免编译器报错。
+    // 在Rust中，PhantomData<T>通常用于在泛型类型中指示类型参数T的存在，但在运行时不占用任何空间。
+    // 这样可以在编译时进行类型检查，而不会引入额外的开销。
+    phantom: std::marker::PhantomData<T>,
+}
+
+impl<T> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        match &self.grade {
+            Grade::Numeric(grade) => {
+                format!(
+                    "{} ({}) - achieved a grade of {}",
+                    &self.student_name, &self.student_age, grade
+                )
+            }
+            Grade::Alphabetical(grade) => {
+                format!(
+                    "{} ({}) - achieved a grade of {}",
+                    &self.student_name, &self.student_age, grade
+                )
+            }
+        }
     }
 }
 
@@ -37,10 +61,11 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card: ReportCard<f32> = ReportCard {
+            grade: Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
+            phantom: std::marker::PhantomData,
         };
         assert_eq!(
             report_card.print(),
@@ -51,10 +76,11 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card: ReportCard<String> = ReportCard {
+            grade: Grade::Alphabetical("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
+            phantom: std::marker::PhantomData,
         };
         assert_eq!(
             report_card.print(),
